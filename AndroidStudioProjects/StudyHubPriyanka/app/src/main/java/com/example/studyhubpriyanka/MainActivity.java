4631a371fe82ab35c9c145cdc62ef9b8577d7d88
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2021-04-17 09:08:01 +0530

    added studyhub project

---
+++ AndroidStudioProjects/StudyHubPriyanka/app/src/main/java/com/example/studyhubpriyanka/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.studyhubpriyanka;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.BottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+    }
+}
